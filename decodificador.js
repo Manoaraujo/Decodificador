c9250ae75b71fc7d191f41b4b516dcbd658672a0
Mano Araujo <manoaraujo@gmail.com>
2023-04-25 20:52:19 -0300

    funções  de codificação atualizadas

--- decodificador.js
+++ decodificador.js
@@ -1,37 +1,40 @@
 
-let codigo = "sera que deu certo?"
 
 // let letrasSubstituir = /a|e|i|o|u/gi
 // let cifras = /'ai'|'enter'|'imes'|'ober'|'ufat'/
 // const x = ['a', 'e', 'i', 'o', 'u']
 // const y = ['ai', 'enter', 'imes', 'ober', 'ufat']
 
-function codificar() {
+let letraU;
+let cifraU
+
+function codificar(codigo) {
 
     let letraE = codigo.replace(/e/gi, "enter")
     let letraI = letraE.replace(/i/gi, "imes")
     let letraA = letraI.replace(/a/gi, "ai")
     let letraO = letraA.replace(/o/gi, "ober")
-    let letraU = letraO.replace(/u/gi, "ufat")
-    console.log(letraU);
+    return letraU = letraO.replace(/u/gi, "ufat")
+
 }
 
-function decodificar() {
+function decodificar(codificado) {
 
     let cifraE = codificado.replace(/enter/gi, "e")
     let cifraI = cifraE.replace(/imes/gi, "i")
     let cifraA = cifraI.replace(/ai/gi, "a")
     let cifraO = cifraA.replace(/ober/gi, "o")
-    let cifraU = cifraO.replace(/ufat/gi, "u")
-    console.log(cifraU);
+    return cifraU = cifraO.replace(/ufat/gi, "u")
+
 }
 
-codificar();
 
-let codificado = 'senterrai qufatenter denterufat centerrtober ?'
+letraU = codificar("germano")
+console.log(letraU);
 
-decodificar();
+decodificar(letraU);
+console.log(cifraU);
 
-
+// codificar("germano");
 // let mensagemSecreta = codigo.replace(letrasSubstituir, "x")
 // console.log(mensagemSecreta)
