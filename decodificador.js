829399a6c2b5f44265d534c88b59ea1eb36b50c0
Mano Araujo <manoaraujo@gmail.com>
2023-04-25 17:45:01 -0300

    criação funções de codificar e descodificar

--- decodificador.js
+++ decodificador.js
@@ -1,2 +1,37 @@
 
-let textoNormal = document.querySelector("textoNormal")
+let codigo = "sera que deu certo?"
+
+// let letrasSubstituir = /a|e|i|o|u/gi
+// let cifras = /'ai'|'enter'|'imes'|'ober'|'ufat'/
+// const x = ['a', 'e', 'i', 'o', 'u']
+// const y = ['ai', 'enter', 'imes', 'ober', 'ufat']
+
+function codificar() {
+
+    let letraE = codigo.replace(/e/gi, "enter")
+    let letraI = letraE.replace(/i/gi, "imes")
+    let letraA = letraI.replace(/a/gi, "ai")
+    let letraO = letraA.replace(/o/gi, "ober")
+    let letraU = letraO.replace(/u/gi, "ufat")
+    console.log(letraU);
+}
+
+function decodificar() {
+
+    let cifraE = codificado.replace(/enter/gi, "e")
+    let cifraI = cifraE.replace(/imes/gi, "i")
+    let cifraA = cifraI.replace(/ai/gi, "a")
+    let cifraO = cifraA.replace(/ober/gi, "o")
+    let cifraU = cifraO.replace(/ufat/gi, "u")
+    console.log(cifraU);
+}
+
+codificar();
+
+let codificado = 'senterrai qufatenter denterufat centerrtober ?'
+
+decodificar();
+
+
+// let mensagemSecreta = codigo.replace(letrasSubstituir, "x")
+// console.log(mensagemSecreta)
